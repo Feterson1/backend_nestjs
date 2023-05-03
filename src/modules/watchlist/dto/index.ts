9e67dfe3759cba511500ba89c9f231e8e3ee2ee3
Feterson <feterson@bk.ru>
2023-05-03 19:12:43 +0600

    updated api swagger ui

--- src/modules/watchlist/dto/index.ts
+++ src/modules/watchlist/dto/index.ts
@@ -1,9 +1,13 @@
+import { ApiProperty } from "@nestjs/swagger"
 import { IsString } from "class-validator"
 
 export class WatchListDTO{
+
+    @ApiProperty()
     @IsString()
     name: string
 
+    @ApiProperty()
     @IsString()
     assetId:string
 }
--- src/modules/watchlist/watchlist.controller.ts
+++ src/modules/watchlist/watchlist.controller.ts
@@ -3,20 +3,31 @@ import { WatchlistService } from './watchlist.service';
 import { WatchListDTO } from './dto';
 import { JwtAuthGuard } from 'src/guards/jwt-guard';
 import { request } from 'http';
+import { CreateAssetResponse } from './response';
+import { ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('watchlist')
 export class WatchlistController {
     constructor(private readonly watchlistService: WatchlistService){}
     
+    @ApiTags('API')
+    @ApiResponse({
+        status:201,
+        type:CreateAssetResponse,
+
+    })
     @UseGuards(JwtAuthGuard)
     @Post('create')
-    createAsset(@Body() assetDTO: WatchListDTO, @Req() request){
+    createAsset(@Body() assetDTO: WatchListDTO, @Req() request): Promise<CreateAssetResponse>{
         const user = request.user;
         return this.watchlistService.createAsset(user,assetDTO);
     }
 
     
-
+    @ApiTags('API')
+    @ApiResponse({
+        status:200,
+    })
     @UseGuards(JwtAuthGuard)
     @Delete()
     deleteAsset(@Query('id') assetId:string, @Req() request):Promise<boolean>{
